8b03224a01deb1c7f2a54af8205fe6a5de34ed87
ethereal element <vitalik.safonov@list.ru>
2023-11-10 11:01:38 +0300

    feat: ui-input

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Input } from './components/ui/input/Input'
 
 export default function Home() {
   return (
    <>
-   
+   <Input type='email' value='hello' size='large' placeholder="Enter your"></Input>
    </>
   )
 }
